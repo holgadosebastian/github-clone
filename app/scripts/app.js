c17cd519600796db6d25335632ae9be80a9bcd2a
Sebastian Holgado <holgado.sebastian@gmail.com>
2018-02-15 17:03:46 -0300

    Init project

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gitCloneApp
+ * @description
+ * # gitCloneApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gitCloneApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitCloneApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gitCloneApp
+ */
+angular.module('gitCloneApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
