d9dd3e67e7e5c1b528d32d543b39716b8da88b5c
Sebastian Holgado <holgado.sebastian@gmail.com>
2018-02-16 11:35:29 -0300

    Built project

--- gulpfile.js
+++ gulpfile.js
@@ -174,8 +174,6 @@ gulp.task('client:build', ['html', 'styles'], function () {
     .pipe(cssFilter)
     .pipe($.minifyCss({cache: true}))
     .pipe(cssFilter.restore())
-    .pipe($.rev())
-    .pipe($.revReplace())
     .pipe(gulp.dest(yeoman.dist));
 });
 
